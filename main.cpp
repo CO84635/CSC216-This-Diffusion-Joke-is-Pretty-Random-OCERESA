dc4e045f6ef8e21354ce7839b70bd2412869cfbd
Owen Ceresa <co84635@mail.harpercollege.edu>
2025-01-26 14:10:01 -0600

    Added additional logic towards making sure my output stayed consistent.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <random>
 #include <ctime>
+#include <cmath>
 #include <vector>
 
 using namespace std;
@@ -15,7 +16,7 @@ int main() {
 
     for (int i = 0; i < M; i++) {
         
-        N = (i < 5) ? 10 : 100;
+        N = (i < (int)ceil(M / 2)) ? 10 : 100;
         int currentPosition = 0;
 
         for (int j = 0; j < N; j++) {
@@ -27,7 +28,7 @@ int main() {
     }
 
     for (int i = 0; i < data.size(); i++) {
-        cout << "T" << i + 1 << ": N" << ((i < 5) ? 10 : 100) << ": " << data[i] << endl;
+        cout << "T" << i + 1 << ": N" << ((i < (int)ceil(M / 2)) ? 10 : 100) << ": " << data[i] << endl;
     }
 
     return 0;
